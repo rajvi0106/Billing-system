07d44e8487aaf10d7d7d99a9ac5269ecdb586975
rajvi0106 <rajvisinghrathore001@gmail.com>
2026-08-04 21:01:15 +0530

    minor changes

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -10,11 +10,11 @@ create table if not exists usage_events(
     event_id  text unique not null,
     customer_id integer not null references customers(id),
     event_type text not null,
-    occured_at timestamptz not null,
-    recived_at timestamptz not null default now(),
-    created_type timestamptz not null default now()
+    occurred_at timestamptz not null,
+    received_at timestamptz not null default now(),
+    created_at timestamptz not null default now()
 );  
-create index if not exists idx_usage_events_customer_period on usage_events(customer_id,occured_at);
+create index if not exists idx_usage_events_customer_period on usage_events(customer_id,occurred_at);
 
 create table if not exists invoices(
     id serial primary key,
